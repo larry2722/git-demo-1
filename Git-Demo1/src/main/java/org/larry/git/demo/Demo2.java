0ca74edd952b0e568b523d5675d1680230970115
larry <libin2722@sohu.com>
2013-12-10 15:34:58 +0800

    add Demo2.java

---
+++ Git-Demo1/src/main/java/org/larry/git/demo/Demo2.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.larry.git.demo;
+
+/**
+ * @author larry
+ *
+ */
+public class Demo2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
